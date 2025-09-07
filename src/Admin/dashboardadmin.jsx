d8c76582740c822a2bc985c2173e4eff3a5bcc3a
lavinalinda <roslindaalvina@gmail.com>
2025-09-07 10:44:12 +0700

    baru

--- src/Admin/dashboardadmin.jsx
+++ src/Admin/dashboardadmin.jsx
@@ -43,7 +43,7 @@ export default function DashboardAdmin() {
         </header>
 
         <button className="bg-black text-white px-4 py-2 rounded-md mb-6">
-          Add Product
+          <link> </link>
         </button>
 
         {/* Product Table */}
--- src/Admin/ordermanagement.jsx
+++ src/Admin/ordermanagement.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { FaHome, FaUsers, FaBoxOpen, FaList } from "react-icons/fa";
 
-export default function OrderManagement () {
+export default function OrderManagement() {
   return (
-    <div>OrderManagement</div>
+    <div className="flex min-h-screen bg-gray-100">
+      {/* Sidebar */}
+      <aside className="w-64 bg-[#15803D] p-6 text-white">
+        <h1 className="text-2xl font-bold mb-8">EcoPick</h1>
+        <nav className="space-y-4">
+          <a href="#" className="flex items-center gap-3 hover:text-green-800">
+            <FaHome /> Dashboard
+          </a>
+          <a href="#" className="flex items-center gap-3 hover:text-green-800">
+            <FaList /> Category Management
+          </a>
+          <a href="#" className="flex items-center gap-3 bg-[#355317] p-2 rounded-md">
+            <FaBoxOpen /> Order Management
+          </a>
+          <a href="#" className="flex items-center gap-3 hover:text-green-800">
+            <FaUsers /> User Management
+          </a>
+        </nav>
+      </aside>
+    </div>
   )
 }
 
